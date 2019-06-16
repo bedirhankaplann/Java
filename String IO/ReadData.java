b5c0d54a9d9ffc9d2f65b7a3edaff5da8cdd44b1
bedirhankaplann <34594057+bedirhankaplann@users.noreply.github.com>
2019-06-17 02:48:15 +0300

    Rename ReadData.txt to ReadData.java

--- String IO/ReadData.java
+++ String IO/ReadData.java
@@ -8,15 +8,15 @@ public class ReadData
     {
         File file = new File("writedata.txt")
         Scanner input = new Scanner(file);
-        //Okuma yapacagiz ama klavyeden degil file dan yapacagız. (File = writedata.txt)
+        //Okuma yapacagiz ama klavyeden degil file dan yapacagÄ±z. (File = writedata.txt)
 
         while ( input.hasNext() ) //Kelime sonuna kadar oku diyoruz. hasNextLine ise satir sonuna kadar.
         {
-            String firstName = input.next(); //İlk okuduğunu firstName degiskenine atadi.
+            String firstName = input.next(); //Ä°lk okuduÄŸunu firstName degiskenine atadi.
             String lastName = input.next(); // Sonrakini  lastName degiskenine atadi.
             int year = input.nextInt(); // Sonuncusunu ise year degiskenine atadi.
 
-            System.out.println(firstName + " " + lastName + " " + year); //Okuduklarımızı ekrana yazıyoruz.
+            System.out.println(firstName + " " + lastName + " " + year); //OkuduklarÄ±mÄ±zÄ± ekrana yazÄ±yoruz.
         }
         input.close(); // Okumayi kapatiyoruz.
     }
